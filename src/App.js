4098f16f8f815be72a5605996b3ee99bcd4c7d06
Estefi <estefania.fabian@hotmail.com>
2022-02-03 03:59:24 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { NavBar } from "../src/core/components/NavBar";
+import './App.css'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
+
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/core/components/NavBar.jsx
@@ -0,0 +1,51 @@
+
+import { useState } from 'react';
+import '../components/navBar.css';
+import logoTravelling from '../../images/logoTravelling.png'
+
+
+
+export function NavBar() {
+
+    const [active, setActive] = useState('nav_menu');
+
+    const [toggleIcon, setToggleIcon] = useState('nav_toggler');
+
+    const handleLogo = () => {
+        // Función que a hacer click en el logo, nos lleve a la página de inicio.
+    }
+
+    const navToggle = () => {
+        active === 'nav_menu'
+            ? setActive('nav_menu nav_active')
+            : setActive('nav_menu')
+
+        // TogglerIcon 
+        toggleIcon === 'nav_toggler'
+            ? setToggleIcon('nav_toggler toggle')
+            : setToggleIcon('nav_toggler')
+    }
+
+    return (
+        <nav className='nav'>
+            <div className='logo'>
+                <img src={logoTravelling} onClick={handleLogo}></img>
+            </div>
+            <ul className={active}>
+                <li className='nav_item'><a href='#' className='nav_link'>INICIO</a></li>
+                <li className='nav_item'><a href='#' className='nav_link'>CONÓCENOS</a></li>
+                <li className='nav_item'><a href='#' className='nav_link'>SERVICIOS</a></li>
+                <li className='nav_item'><a href='#' className='nav_link'>ARTISTAS</a></li>
+                <li className='nav_item'><a href='#' className='nav_link'>CONTACTO</a></li>
+                <li className='nav_item'><a href='#' className='nav_link'>ACCESO</a></li>
+            </ul>
+
+            <div onClick={navToggle} className={toggleIcon}>
+                <div className='line1'></div>
+                <div className='line2'></div>
+                <div className='line3'></div>
+            </div>
+
+        </nav>
+    )
+}
