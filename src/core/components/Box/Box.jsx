2542e7e0ca8bc49ec865dc8af1a7b46f01805c4a
Jhony Dev <iamjareco@gmail.com>
2022-06-21 18:59:09 +0200

    Renombrado className con metodologia BEM

--- src/core/components/Box/Box.jsx
+++ src/core/components/Box/Box.jsx
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 import "./box.css";
 
-const Box = ({ children, className = "box-container" }) => {
+const Box = ({ children, className = "box__div--container" }) => {
 	return (
 		<>
 			<div className={className}>{children}</div>
