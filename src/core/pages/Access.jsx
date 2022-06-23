88b726bef254b2296c751d13a1ac2b01bbfd94dc
Jhony Dev <iamjareco@gmail.com>
2022-06-23 20:39:09 +0200

    Modificacion componente page Access

--- src/core/pages/Access.jsx
+++ src/core/pages/Access.jsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
 import Login from "core/components/Login/Login";
+import Register from "core/components/Register/Register";
 
 export default function Access() {
+	const [toggle, setToggle] = useState(false);
+	
 	return (
 		<div>
 			<div>
 				<h1>Contenido página Acceso</h1>
-				<Login />
+				{toggle ? <Login /> : <Register />}				
 			</div>
 		</div>
 
