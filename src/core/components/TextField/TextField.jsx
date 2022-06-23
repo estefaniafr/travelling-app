a9462b8c34074a05a75833a33134b03a97e30c71
Jhony Dev <iamjareco@gmail.com>
2022-06-23 20:35:54 +0200

    Creación componente TextField

---
+++ src/core/components/TextField/TextField.jsx
@@ -0,0 +1,32 @@
+import PropTypes from "prop-types";
+
+import Box from "../Box/Box";
+import "./TextField.css";
+
+const TextField = ({ className = "text-field__box--container", name, type = "text", onChange, label, value }) => {    
+	return (
+		<Box className={className}>
+			<label htmlFor={name}>{label}</label>
+			<input
+				className="text-field__input--element"
+				id={name}
+				name={name}
+				type={type}
+				onChange={onChange}
+				value={value}
+			/>
+		</Box>
+
+	);
+};
+
+TextField.propTypes = {
+	className: PropTypes.string,
+	name: PropTypes.string.isRequired,
+	type: PropTypes.string,
+	label: PropTypes.string.isRequired,
+	value: PropTypes.string,
+	onChange: PropTypes.func,
+};
+
+export default TextField;
