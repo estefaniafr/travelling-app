0700647b7d26d2e17afad50e78fbead5de1f97a5
Estefania <estefania.fabian@hotmail.com>
2022-06-25 17:43:40 +0200

    Create componnent button

---
+++ src/core/components/Button/Button.jsx
@@ -0,0 +1,39 @@
+import PropTypes from "prop-types";
+import "./Button.css";
+
+const Button = ({
+	className = "button__button--wrapper",
+	type = "button",
+	kind = "primary",
+	value = "button",
+}) => {
+	const getKindStyle = () => {
+		switch (kind) {
+			case "primary":
+				return `${className} btn-5`;
+			case "secondary":
+				return `${className} btn-1`;
+			default:
+				return `${className} btn-13`;
+		}
+	};
+	return (
+		<button className={getKindStyle()} type={type}>
+			{value}
+		</button>
+	);
+};
+
+Button.defaultProps = {
+	onClick: () => ({}),
+};
+
+Button.propTypes = {
+	className: PropTypes.string,
+	type: PropTypes.string,
+	kind: PropTypes.string,
+	value: PropTypes.string,
+	onClick: PropTypes.func,
+};
+
+export default Button;
