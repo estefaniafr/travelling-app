d15c22b2e2d112aeacebfecf385a5d38329799be
Estefania <estefania.fabian@hotmail.com>
2022-06-25 17:27:06 +0200

    Added className as props

--- src/core/components/Box/Box.jsx
+++ src/core/components/Box/Box.jsx
@@ -2,15 +2,18 @@ import PropTypes from "prop-types";
 import "./box.css";
 
 const Box = ({ children }) => {
+const Box = ({ children, className = "box-container" }) => {
 	return (
 		<>
 			<div className="box-container">{children}</div>
+			<div className={className}>{children}</div>
 		</>
 	);
 };
 
 Box.propTypes = {
 	children: PropTypes.node.isRequired,
+	className: PropTypes.string.isRequired,
 };
 
 export default Box;
