ad074602554afe7e6da235bb8a7384e5aa7696ac
Estefania <estefania.fabian@hotmail.com>
2022-06-25 17:57:15 +0200

    Login form modification

--- src/core/components/login/Login.jsx
+++ src/core/components/login/Login.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import { useFormik } from "formik";
 import Box from "../Box/Box";
+import api from "core/api";
+
+const initialValues = {
+	email: undefined,
+	password: undefined,
+};
 
 const Login = () => {
-	return (
-		<Box>
-			<h3>Login</h3>
-		</Box>
-	);
+	const formik = useFormik({
+		initialValues,
+		onSubmit: async (values) => {
+			try {
+				const isLogged = await api.post({ path: "/login" });
+				if (isLogged) {
+					console.log(values);
+				}
+			} catch (error) {
+				console.log(error);
+			}
+		},
+	});
 };
