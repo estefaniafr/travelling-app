069dc7d58e9fb5a32bd8c03335f28d43716cec74
Jhony Dev <iamjareco@gmail.com>
2022-07-01 23:44:35 +0200

    Refactorizacion componente Register

--- src/core/components/Register/Register.jsx
+++ src/core/components/Register/Register.jsx
@@ -1,10 +1,11 @@
 import { useFormik } from "formik";
-import Box from "../Box/Box";
-import api from "core/api";
+import useAxiosGet from "core/api/hooks/useAxiosGet";
 
-import "./Register.css";
+import Box from "../Box/Box";
 import TextField from "../TextField/TextField";
 import Button from "../Button/Button";
+
+import "./Register.css";
 
 const initialValues = {
 	name: "",
@@ -14,64 +15,63 @@ const initialValues = {
 	phone: "",
 };
 
-const Register = () => {
+const Register = ({ setToggle }) => {
+	const { data, loaded } = useAxiosGet("/users");
 	const formik = useFormik({
 		initialValues,
 		validateOnChange: () => console.log("validate"),
 		onSubmit: async (values) => {
-			const isLogged = await api.get({ path: "/register" });
-			if (isLogged) {
+			if (!loaded) {
 				console.log(values);
 			}
 		},
 	});
-    
+
 	return (
-		<Box className="register__box--container">
-			<form onSubmit={formik.handleSubmit} onReset={formik.handleReset}>
-				<h3>Registro</h3>	
-				<TextField 
-					name="name" 
-					label="Nombre"
-					onChange={formik.handleChange}
-					value={formik.values.name}
-				/>
-				<TextField 
-					name="lastname" 
-					label="Apellidos"
-					onChange={formik.handleChange}
-					value={formik.values.lastname}
-				/>
-				<TextField 
-					name="email" 
-					label="Email"
-					type="email" 
-					onChange={formik.handleChange}
-					value={formik.values.email}
-				/>
-				<TextField 
-					name="password" 
-					label="Contraseña" 
-					type="password" 
-					onChange={formik.handleChange}
-					value={formik.values.password}
-				/>
-				<TextField 
-					name="phone" 
-					label="Telefono" 
-					type="tel" 
-					onChange={formik.handleChange}
-					value={formik.values.phone}
-				/>
+		<form onSubmit={formik.handleSubmit} onReset={formik.handleReset}>
+			<h3>Registro</h3>
+			<TextField
+				name="name"
+				label="Nombre"
+				onChange={formik.handleChange}
+				value={formik.values.name}
+			/>
+			<TextField
+				name="lastname"
+				label="Apellidos"
+				onChange={formik.handleChange}
+				value={formik.values.lastname}
+			/>
+			<TextField
+				name="email"
+				label="Email"
+				type="email"
+				onChange={formik.handleChange}
+				value={formik.values.email}
+			/>
+			<TextField
+				name="password"
+				label="Contraseña"
+				type="password"
+				onChange={formik.handleChange}
+				value={formik.values.password}
+			/>
+			<TextField
+				name="phone"
+				label="Telefono"
+				type="tel"
+				onChange={formik.handleChange}
+				value={formik.values.phone}
+			/>
+			<a href="#" onClick={() => setToggle((old) => !old)}>
+				Tienes una cuenta? Inicia sesión
+			</a>
 
-				<Box className="register__box--buttons">
-					<Button  kind="secondary" type="reset" value="Borrar datos" />	
-					<Button type="submit" value="Registrar" />
-				</Box>
-            
-			</form>
-		</Box>
-
+			<Box className="register__box--buttons">
+				<Button kind="secondary" type="reset" value="Borrar datos" />
+				<Button type="submit" value="Registrar" />
+			</Box>
+		</form>
 	);
 };
 
