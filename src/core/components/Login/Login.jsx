74b4d20d901b03a061c32a04c5d966b61d4f739d
Jhony Dev <iamjareco@gmail.com>
2022-07-01 23:44:14 +0200

    Refactorizacion componente Login

--- src/core/components/Login/Login.jsx
+++ src/core/components/Login/Login.jsx
@@ -1,52 +1,67 @@
-import { useFormik } from "formik";
+import { Formik, Form, Field } from "formik";
 import Box from "../Box/Box";
-import api from "core/api";
+import Button from "../Button/Button";
 
-const initialValues = {
-	email: undefined,
-	password: undefined,
-};
+import "./Login.css";
 
-const Login = () => {
-	const formik = useFormik({
-		initialValues,
-		onSubmit: async (values) => {
-			try {
-				const isLogged = await api.post({ path: "/login" });
-				if (isLogged) {
-					console.log(values);
-				}				
-			} catch (error) {
-				console.log(error);
-			}
-		},
-	});
-    
+export const Login = ({ setToggle }) => {
+	const validateEmail = (value) => {
+		let error;
+		if (!value) {
+			error = "Requerido";
+		} else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)) {
+			error = "No es un email valido";
+		}
+		return error;
+	};
+
+	const validateUsername = (value) => {
+		let error;
+		if (!value) {
+			error = "Requerido";
+		}
+		return error;
+	};
+
 	return (
-		<Box>
-			<h3>Login</h3>
-			<form onSubmit={formik.handleSubmit}>
-				<label htmlFor="email">Email Address</label>
-				<input
-					id="email"
-					name="email"
-					type="email"
-					onChange={formik.handleChange}
-					value={formik.values.email}
-				/>
-				<label htmlFor="password">Password</label>
-				<input
-					id="password"
-					name="password"
-					type="password"
-					onChange={formik.handleChange}
-					value={formik.values.password}
-				/>
-            
-				<button type="submit">Submit</button>
-			</form>
-		</Box>
+		<Formik
+			initialValues={{
+				email: "",
+				password: "",
+			}}
+			onSubmit={(values) => {
+				// same shape as initial values
+				console.log(values);
+			}}
+		>
+			{({ errors, touched, validateField, validateForm }) => (
+				<Form>
+					<h1>Login</h1>
+					<Field
+						className="login__field--element"
+						name="email"
+						validate={validateEmail}
+					/>
+					{errors.email && touched.email && <Box>{errors.email}</Box>}
 
+					<Field
+						className="login__field--element"
+						type="password"
+						name="password"
+						validate={validateUsername}
+					/>
+					{errors.password && touched.password && <Box>{errors.password}</Box>}
+
+					<a href="#" onClick={() => setToggle((old) => !old)}>
+						No tienes cuenta? Regístrate
+					</a>
+
+					<Box className="login__box--buttons">
+						<Button type="submit" value="Login" />
+					</Box>
+				</Form>
+			)}
+		</Formik>
 	);
 };
 
