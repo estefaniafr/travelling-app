313ef810a8047ddd8bfc3384684147adfc4be12c
Jhony Dev <iamjareco@gmail.com>
2022-07-12 18:54:07 +0200

    Anadir HOC forwardRef a el componente box

--- src/core/components/Box/Box.jsx
+++ src/core/components/Box/Box.jsx
@@ -1,19 +1,22 @@
+import { forwardRef } from "react";
 import PropTypes from "prop-types";
 import "./Box.css";
 
-const Box = ({ children, className = "box__div--container", ...props }) => {
+const Box = forwardRef(({ children, ...props }, ref) => {
 	return (
 		<>
-			<div {...props} className={className}>
+			<div ref={ref} {...props}>
 				{children}
 			</div>
 		</>
 	);
-};
+});
 
 Box.propTypes = {
 	children: PropTypes.node.isRequired,
 	className: PropTypes.string,
 };
+
+Box.displayName = "Box";
 
 export default Box;
