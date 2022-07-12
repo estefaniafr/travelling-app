f4e0efb0c1e4799f7cf1afd88111a68b5b5042ef
Jhony Dev <iamjareco@gmail.com>
2022-07-12 19:00:54 +0200

    Implementar hook useOutsideClick en el componente Sidebar

--- src/core/components/Sidebar/Sidebar.jsx
+++ src/core/components/Sidebar/Sidebar.jsx
@@ -1,27 +1,39 @@
-import { memo, useEffect } from "react";
+import { memo, useEffect, useRef } from "react";
 import PropTypes from "prop-types";
 import Box from "core/components/Box/Box";
 import "./Sidebar.css";
+import useOutsideClick from "core/hooks/useOutsideClick";
 
 const Sidebar = ({ isOpen, side, onClose, children }) => {
+	const rootElement = document.getElementById("root");
+	const sidebarRef = useRef(null);
+
+	const initialValues = {
+		transition: rootElement.style.transition,
+		backgroundColor: rootElement.style.backgroundColor,
+	};
+
 	const classBuilder = () => {
 		return `sidebar__box--container ${isOpen ? "--open" : ""}
         `;
 	};
 
+	useOutsideClick(sidebarRef, onClose);
+
 	useEffect(() => {
 		if (isOpen) {
-			document.getElementById("root").style.transition = "background-color .5s";
-			document.getElementById("root").style.backgroundColor = "rgba(0,0,0,0.4)";
+			rootElement.style.transition = "background-color .5s";
+			rootElement.style.backgroundColor = "rgba(0,0,0,0.4)";
 		}
 
 		return () => {
-			document.getElementById("root").style.backgroundColor = null;
+			rootElement.style.backgroundColor = initialValues.backgroundColor;
+			rootElement.style.transition = initialValues.transition;
 		};
 	}, [isOpen]);
 
 	return (
-		<Box style={{ [side]: 0 }} className={classBuilder()}>
+		<Box ref={sidebarRef} style={{ [side]: 0 }} className={classBuilder()}>
 			<span className="sidebar__span--close" onClick={onClose}>
 				&times;
 			</span>
