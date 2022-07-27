b392f64d5ee3f01bb18be49e823a53b6600c2d73
Estefania <estfruiz@gmail.com>
2022-07-27 16:41:18 +0200

    creating about page

--- src/about/pages/About.jsx
+++ src/about/pages/About.jsx
@@ -1,11 +1,26 @@
+import Box from "core/components/Box/Box";
+import "./About.css";
+
 export default function About() {
 	return (
 
-		<div>
-			<div>
-				<h1>Contenido página Sobre nosotros</h1>
-			</div>
-		</div>
+		<Box className="about-page__box--container">        
+			<Box className="about-page__box--image">
+				<img src="https://images.pexels.com/photos/4032062/pexels-photo-4032062.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+			</Box>
+			<Box className="about-page__box--text">
+				<h1>SOBRE NOSOTROS</h1>
+				<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim cupiditate reiciendis harum maiores rerum possimus ducimus sunt corrupti aliquid debitis expedita, itaque autem incidunt consectetur veritatis provident amet, ad deleniti! Recusandae culpa laborum eos velit id harum facere excepturi voluptates mollitia provident officiis labore sequi quos architecto doloribus, corrupti aliquid, incidunt cum blanditiis a aspernatur distinctio nesciunt. Ab non provident earum ex. Ullam quidem at cum necessitatibus officiis provident et magni modi nihil voluptates iusto, cupiditate enim iste temporibus quibusdam possimus aut, ad voluptatum, inventore quae eius laboriosam neque totam animi! Ratione recusandae sint optio dolorum dignissimos laborum deserunt id?        </p>
+			</Box>
+			<Box className="about-page__box--middel-image">
+				<img src="https://images.pexels.com/photos/3184395/pexels-photo-3184395.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+			</Box>
+			<Box className="about-page__box--text">
+				<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim cupiditate reiciendis harum maiores rerum possimus ducimus sunt corrupti aliquid debitis expedita, itaque autem incidunt consectetur veritatis provident amet, ad deleniti! Recusandae culpa laborum eos velit id harum facere excepturi voluptates mollitia provident officiis labore sequi quos architecto doloribus, corrupti aliquid, incidunt cum blanditiis a aspernatur distinctio nesciunt. Ab non provident earum ex. Ullam quidem at cum necessitatibus officiis provident et magni modi nihil voluptates iusto, cupiditate enim iste temporibus quibusdam possimus aut, ad voluptatum, inventore quae eius laboriosam neque totam animi! Ratione recusandae sint optio dolorum dignissimos laborum deserunt id?        </p>
+			</Box>
+            
+            
+		</Box>
 
 	);
 }
